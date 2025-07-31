b0f17997d54d6d7f9a82b38d27febc2fd4a26e28
Yell Min Naing <117013435+YellMinNaing-micro@users.noreply.github.com>
2025-07-31 16:05:39 +0630

    Update app.routes.ts

--- my-ionic-app/src/app/app.routes.ts
+++ my-ionic-app/src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { TodoComponent } from "./pages/todo/todo.component";
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  // },
+
+  // { path: '', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'todo', component: TodoComponent },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    loadComponent: () => import('./login-page/login-page.page').then( m => m.LoginPagePage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register-page/register-page.page').then( m => m.RegisterPagePage)
+  },
+  {
+    path: 'todo',
+    loadComponent: () => import('./todo-page/todo-page.page').then( m => m.TodoPagePage)
   },
 ];
